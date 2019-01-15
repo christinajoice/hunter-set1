168a5d6db231facd4b7d834e43131c509a24b7f2
christinajoice <christinallrejoice@gmail.com>
2019-01-15 08:01:47 -0800

    Update IndexEqual.java

--- IndexEqual.java
+++ IndexEqual.java
@@ -6,7 +6,7 @@ public static void main(String args[])
 {
 Scanner s=new Scanner(System.in);
 int a[]=new int[10];int i,j;
-int newa;
+int newa[]=new int[10];
 System.out.println("Enter the number of values");
 int n=s.nextInt();
 System.out.println("Enter the values");
@@ -16,16 +16,17 @@ a[i]=s.nextInt();
 }
 for(i=0;i<n;i++)
 {
+  for(j=0;j<j+1;)
+  {
 if(i==a[i])
 {
-newa=newa+" "+a[i];
+  newa[j]=a[i];
 }
 }
-System.out.println(newa);
-/*
-for(i=0;i<;i++)
+}
+for(i=0;i<newa.length-1;i++)
 {
-for(j=1;j<;j++)
+for(j=1;j<newa.length;j++)
 {
 if(newa[i]>newa[j])
 {
@@ -37,6 +38,6 @@ newa[j]=temp;
 System.out.println("Index equal values are");
 for(i=0;i<n;i++)
 System.out.print(newa[i]);
-}*/
+}
 }
 }
