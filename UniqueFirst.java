86e060806936d824dcd253d03b8d44625ffef2ba
christinajoice <christinallrejoice@gmail.com>
2019-01-15 20:15:12 -0800

    Create UniqueFirst.java

---
+++ UniqueFirst.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java,util,*;
+public class UniqueFirst
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+System.out.println("Enter number of elements");
+int n=s.nextInt();
+System,out,println("Enter values");
+for(i=0;i<n;i++)
+{
+a[i]=s.nextInt();
+}
+out:
+for(i=0;i<n;i++)
+{
+in:
+for(j=0;j<n && i!=j;j++)
+{
+if(a[i]==a[j])
+{
+System.out.println("the first element is"+a[i]);
+break;
+break out;
+break in;
+}
+}
+}
+}
+}
