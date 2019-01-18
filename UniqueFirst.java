7a36342450dc542632fe91e6ea3b2238724ad412
christinajoice <christinallrejoice@gmail.com>
2019-01-18 11:42:09 +0530

    Update UniqueFirst.java

--- UniqueFirst.java
+++ UniqueFirst.java
@@ -1,13 +1,15 @@
 import java.io.*;
-import java,util,*;
+import java.util.*;
 public class UniqueFirst
 {
 public static void main(String args[])
 {
 Scanner s=new Scanner(System.in);
+  int a[]=new int[10];
+  int i,j;
 System.out.println("Enter number of elements");
 int n=s.nextInt();
-System,out,println("Enter values");
+System.out.println("Enter values");
 for(i=0;i<n;i++)
 {
 a[i]=s.nextInt();
@@ -21,9 +23,9 @@ for(j=0;j<n && i!=j;j++)
 if(a[i]==a[j])
 {
 System.out.println("the first element is"+a[i]);
-break;
+//break;
 break out;
-break in;
+//break in;
 }
 }
 }
