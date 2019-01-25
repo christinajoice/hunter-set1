89c7a6e6640153e50116a9d7ab458beda134d743
christinajoice <christinallrejoice@gmail.com>
2019-01-25 11:56:57 +0530

    Create Possible.java

---
+++ Possible.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+public class Possible
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int n,i,j,k;
+int a[]=new int[10];
+System.out.println("Enter the number of elements in array");
+n=s.nextInt();
+System.out.println("Enter the elements");
+for(i=0;i<n;i++)
+{
+a[i]=s.nextInt();
+}
+for(i=0;i<n;i++)
+{
+for(j=0;j<n;j++)
+{
+for(k=0;k<n;k++)
+{
+if(i!=j)
+{
+if(a[i]+a[j]==a[k])
+{
+System.out.println(a[i]+" "+a[j]+" "+a[k]);
+}
+}
+}
+}
+}
+}
+}
